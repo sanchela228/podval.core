e6c04e341f202ed491a888f61b7a3f83e64a7c54
sanchela228 <94424298+sanchela228@users.noreply.github.com>
2024-09-03 16:09:33 +0500

    change

--- local/routes/main.php
+++ local/routes/main.php
@@ -5,12 +5,6 @@
 use Bitrix\Main\Application;
 use Bitrix\Main\Loader;
 use Bitrix\Main\Routing\RoutingConfigurator;
-use PAD\Core\Actions\Page;
-use PAD\Core\Interfaces\Action;
-use PAD\Core\Tools\Alias;
-use PAD\Core\Tools\ContainerPath;
-use PAD\Core\Values\Page\Js;
-use PAD\Core\Views;
 
 return function ( RoutingConfigurator $routes )
 {
